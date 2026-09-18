c79e6f28925b7a872cfc7ece75afdc3fd33c10e8
Hugo <hugo.hamon@edu.devinci.fr>
2026-09-18 11:25:39 +0200

    integration des types + debut css

--- pokedex.js
+++ pokedex.js
@@ -1,4 +1,3 @@
-// Création d'une fonction qui affichera les messages d'erreurs
 const displayError = (message) => {
   const error = document.createElement('p')
   error.classList.add('red')
@@ -6,58 +5,67 @@ const displayError = (message) => {
   document.body.appendChild(error)
 }
  
-// Création d'une fonction qui appelle l'API
 const getData = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
     .catch(error => {
       console.error("Error:", error)
       displayError("Une erreur est survenue")
     });
-    // On prépare une liste vide et l'index des cartes
         
     let pokedex_list = [];
     let index = 1;
 
-     if(response.status < 300){
-        // On récupère le corps de la requête
+    if(response.status < 300){
         const data = await response.json();
-        data.results.forEach(element => {
         
-        pokedex_list.push([
-            index,
-            element.name,
-            element.url,
-            `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`
-        ]);
-        index += 1;
-        });
-    }else{
-    // Status code >= 300
-    displayError("Une erreur est survenue")
-  }
-    return pokedex_list; // retourne la liste pour obtenir les information
+        // On utilise une boucle for...of pour pouvoir faire des fetch sur chaque pokemon
+        for (const element of data.results) {
+            // On récupère les détails du pokémon pour avoir ses types
+            const resDetails = await fetch(element.url);
+            const details = await resDetails.json();
+            
+            // On extrait les noms des types (ex: ['grass', 'poison'])
+            const types = details.types.map(t => t.type.name);
 
-  } 
-
+            pokedex_list.push([
+                index,
+                element.name,
+                element.url,
+                `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`,
+                types 
+            ]);
+            index += 1;
+        }
+    } else {
+        displayError("Une erreur est survenue");
+    }
+    return pokedex_list;
+} 
 
 async function creerpokedex(){
     const pokedex_list = await getData();
     let pokedex ="";
-    for (let i =0;i<50;i++){
+    
+    for (let i = 0; i < pokedex_list.length; i++) {
+        
+        let typesHtml = pokedex_list[i][4].map(t => `<span class="badge_type">${t}</span>`).join('');
+
         pokedex += `<div class="carte" name="${pokedex_list[i][0]}">
-                        <div class="case_nom">
-                            <h2>${pokedex_list[i][1]}</h2>
+                        <div class="carte_header">
+                            <div class="case_nom">
+                                <h2>${pokedex_list[i][1]}</h2>
+                            </div>
+                            <div class="affichage_type">
+                                ${typesHtml}
+                            </div>
                         </div>
                         <div class="image_carte">
                             <img src="${pokedex_list[i][3]}" alt="image de ${pokedex_list[i][1]}">
                         </div> 
-                    </div>`
+                    </div>`;
     }
     const pokedex_div = document.getElementById("pokedex");
-    
-    // On injecte le HTML généré à l'intérieur de la div
     pokedex_div.innerHTML = pokedex;
 }
-
 
 creerpokedex();
